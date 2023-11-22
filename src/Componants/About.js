3d92755e055118ee7ae9be04bb56190e13885144
Omkar Ambalal Kodmur <omkarkodmur2031@gmail.com>
2023-11-22 18:42:06 +0530

    error handled

--- src/Componants/About.js
+++ src/Componants/About.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 export default function About() {
   return (
     // <div className='footer ' style={{"overflow-x":"auto;"}}>
