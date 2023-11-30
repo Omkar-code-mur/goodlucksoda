4fddc6274d72714a0c1a51af4e3a09441ba5d4fd
Omkar Ambalal Kodmur <omkarkodmur2031@gmail.com>
2023-11-30 13:13:10 +0530

    names changed

--- src/Componants/Cards.js
+++ src/Componants/Cards.js
@@ -7,28 +7,28 @@ import About from "./Contact";
 import Banner from "./Banner"; 
 export default function Cards(props) {
   const bestSellers = [
-    { title: "Jeera Soda", url: "images/jeera.jpg", value:10  },
-    { title: "Fruit Beer Soda", url: "images/fruitBeer.jpg", value:10  },
-    { title: "Blueberry Soda", url: "images/blueberry.jpg", value:10  },
+    { title: "Jeera", url: "images/jeera.jpg", value:10  },
+    { title: "Fruit Beer", url: "images/fruitBeer.jpg", value:10  },
+    { title: "Blueberry", url: "images/blueberry.jpg", value:10  },
     { title: "Sweet Limbo", url: "images/15/sweetlemon.jpg", value:15  },
   ];
   const sodas = [
-    { title: "Mango Soda", url: "images/mango.jpg", value:10  },
-    { title: "Limca Soda", url: "images/limca.jpg", value:10  },
-    { title: "Kala Khatta ", url: "images/kalaKhatta.jpg", value:10  },
-    { title: "Pudina Soda ", url: "images/pudina.jpg", value:10  },
-    { title: "Orange Soda", url: "images/orange.jpg", value:10  },
-    { title: "Jeera Soda", url: "images/jeera.jpg", value:10  },
-    { title: "Fruit Beer Soda", url: "images/fruitBeer.jpg", value:10  },
-    { title: "Blueberry Soda", url: "images/blueberry.jpg", value:10  },
-    { title: "Lemon Soda ", url: "images/lemon.jpg", value:10  },
-    { title: "Strawberry Soda ", url: "images/strawberry.jpg", value:10  },
-    { title: "Cola Soda", url: "images/cola.jpg", value:10  },
-    { title: "Limbo Masala", url: "images/15/masala.jpg", value:15  },
-    { title: "Sweet Limbo", url: "images/15/sweetlemon.jpg", value:15  },
-    { title: "Salty Limbo", url: "images/15/saltyLemon.jpg", value:15  },
-    { title: "Ginger Limbo", url: "images/15/ginger.jpg", value:15  },
-    { title: "Nimbu Sharbat", url: "images/15/nimbuSharbat.jpg", value:15  },
+    { title: "Mango", url: "images/mango.jpg", value: 10 },
+    { title: "Limca", url: "images/limca.jpg", value: 10 },
+    { title: "Kala Khatta", url: "images/kalaKhatta.jpg", value: 10 },
+    { title: "Orange ", url: "images/orange.jpg", value: 10 },
+    { title: "Pudina ", url: "images/pudina.jpg", value: 10 },
+    { title: "Jeera", url: "images/jeera.jpg", value: 10 },
+    { title: "Fruit Beer", url: "images/fruitBeer.jpg", value: 10 },
+    { title: "Blueberry", url: "images/blueberry.jpg", value: 10 },
+    { title: "Lemon ", url: "images/lemon.jpg", value: 10 },
+    { title: "Strawberry ", url: "images/strawberry.jpg", value: 10 },
+    { title: "Cola", url: "images/cola.jpg", value: 10 },
+    { title: "Limbo Masala", url: "images/15/masala.jpg", value: 15 },
+    { title: "Sweet Limbo", url: "images/15/sweetlemon.jpg", value: 15 },
+    { title: "Salty Limbo", url: "images/15/saltyLemon.jpg", value: 15 },
+    { title: "Ginger Limbo", url: "images/15/ginger.jpg", value: 15 },
+    { title: "Nimbu Sharbat", url: "images/15/nimbuSharbat.jpg", value: 15 },
   ];
   return (
     <div className='cards'>
